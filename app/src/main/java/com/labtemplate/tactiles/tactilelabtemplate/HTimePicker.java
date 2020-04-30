cf168ad8df7f3c4266e4427f348a6be41500f0cf
Alessandro Iop <AleIop@users.noreply.github.com>
2020-04-30 16:26:32 +0200

    Toggle

--- app/src/main/java/com/labtemplate/tactiles/tactilelabtemplate/HTimePicker.java
+++ app/src/main/java/com/labtemplate/tactiles/tactilelabtemplate/HTimePicker.java
@@ -1,12 +1,24 @@
 package com.labtemplate.tactiles.tactilelabtemplate;
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.icu.util.Calendar;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TimePicker;
+
 public class HTimePicker  extends Fragment {
     /**
      * The fragment argument representing the section number for this
@@ -15,6 +27,7 @@ public class HTimePicker  extends Fragment {
     private static final String ARG_SECTION_NUMBER = "section_number";
 
     TextView textView;
+    Vibrator vibr;
 
     public HTimePicker() {
     }
@@ -36,26 +49,50 @@ public class HTimePicker  extends Fragment {
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.hslider, container, false);
-        textView = (TextView) rootView.findViewById(R.id.section_label);
+        textView = (TextView) rootView.findViewById(R.id.htimepicker_textView);
         textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
 
-
-        SeekBar bar = rootView.findViewById(R.id.view1_seekBar);
-
-        bar.setOnSeekBarChangeListener(new OnSeekBarChangeListener(){
-
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser){
-                //This function is called whenever the slider is moved
-            }
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar){
-            }
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar){
-            }
-        });
+        vibr = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
 
         return rootView;
+    }
+
+    public void showTimePickerDialog(View v) {
+        DialogFragment newFragment = new TimePickerFragment(v);
+        //newFragment.show(getSupportFragmentManager(), "timePicker");
+    }
+
+    @SuppressLint("ValidFragment")
+    class TimePickerFragment extends DialogFragment
+            implements TimePickerDialog.OnTimeSetListener {
+
+        View button;
+        final long[] pattern1 = {100, 0, 1000};
+        final long[] pattern2 = {100, 0, 1000};
+        final long[] pattern3 = {100, 0, 1000};
+
+        public TimePickerFragment(View v) {
+            button = v;
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.N)
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current time as the default values for the picker
+            final Calendar c = Calendar.getInstance();
+            int hour = c.get(Calendar.HOUR_OF_DAY);
+            int minute = c.get(Calendar.MINUTE);
+
+            // Create a new instance of TimePickerDialog and return it
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            // Do something with the time chosen by the user
+            if (button.getId() == R.id.timepicker1) vibr.vibrate(pattern1, -1);
+            else if (button.getId() == R.id.timepicker1) vibr.vibrate(pattern2, -1);
+            else if (button.getId() == R.id.timepicker1) vibr.vibrate(pattern3, -1);
+        }
     }
 }
